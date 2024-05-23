76869500bcfd661e00acbe147d02e5123a7d3935
neominal.solutions <neominal.solutions@gmail.com>
2024-05-23 09:30:35 +0300

    Todo Sample

--- src/components/functions/todo-sample.tsx
+++ src/components/functions/todo-sample.tsx
@@ -4,6 +4,7 @@ import { Todo } from './models/todo.model';
 type TodoState = {
 	title: string;
 	completed?: boolean;
+	index?: number;
 };
 
 function TodoSample() {
@@ -12,6 +13,8 @@ function TodoSample() {
 	// formdaki ınpıut alanlarını değiştirip bunu todo[] içerisine push etmek istiyoruz
 	// Ekle butonuna basıldığında todos dizisi içerisine eklemek istiyoruz.
 	const [form, setForm] = useState<TodoState>({ title: '', completed: false });
+
+	const [mode, setMode] = useState('create');
 
 	const onFormSubmit = (e: FormEvent) => {
 		e.preventDefault(); // formu durduruk.
@@ -67,7 +70,28 @@ function TodoSample() {
 					type="checkbox"
 				/>{' '}
 				Tamamlandı Mı ?<br></br>
-				<input type="submit" value={'Ekle'} />
+				{mode === 'create' ? (
+					<input type="submit" value={'Ekle'} />
+				) : (
+					<button
+						onClick={() => {
+							todos.map((item: Todo, index: number) => {
+								// düzenle buttonuna basınca form doldurduk ve düzenlenen seçilen nesne ile todos içerisinde sıralı olan nesnleri index üzerinden bulup, özellilklerini güncelledik.
+								if (index === form.index) {
+									item.title = form.title;
+									item.completed = form.completed as boolean;
+								}
+								return { ...item };
+							});
+
+							setTodos([...todos]); // virtual dom yeni state ekrana basmış oldu.
+							setForm({ title: '', completed: false, index: -1 });
+						}}
+						type="button"
+					>
+						Güncelle
+					</button>
+				)}
 			</form>
 
 			<hr></hr>
@@ -81,11 +105,26 @@ function TodoSample() {
 						{item.title} /{' '}
 						<span style={{ color: item.completed ? 'green' : 'red' }}>
 							{item.completed ? 'Görev Tamamlandı' : 'Görev Beklemede'}
-							<input type="checkbox" />
+							<input
+								disabled={item.completed}
+								checked={item.completed}
+								type="checkbox"
+								onChange={() => {
+									item.completed = !item.completed;
+									setTodos([...todos]); // virtaul dom tetiklenmiş oldu.
+								}}
+							/>
 							{/* checkbox true olunca todos dizindeki item state güncellensin */}
 						</span>
 						<span>
-							<button>Düzenle</button>
+							<button
+								onClick={() => {
+									setForm({ ...item, index: index });
+									setMode('update');
+								}}
+							>
+								Düzenle
+							</button>
 							<button>Sil</button>
 							{/* Düzenle diyince Ekle butonu yerine Update Button görünsün */}
 						</span>
